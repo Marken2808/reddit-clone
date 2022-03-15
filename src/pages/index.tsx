b262b1f05cfccd89f94427013bc1741c02e6bb78
Marken <nctuan.ityu@gmail.com>
2022-03-15 22:29:26 +0000

    fetch list post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
 import { Typography } from "@material-ui/core";
+import { API } from "aws-amplify";
+import { useEffect, useState } from "react";
+import { ListPostsQuery, Post } from "../API";
 import { useUser } from "../context/AuthContext";
+import { listPosts } from "../graphql/queries";
+
 export default function Home() {
   const { user } = useUser();
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPostsFromApi = async (): Promise<Post[]> => {
+      const allPosts = (await API.graphql({ query: listPosts })) as {
+        data: ListPostsQuery;
+        errors: any[];
+      };
+
+      if (allPosts.data) {
+        setPosts(allPosts.data.listPosts.items as Post[]);
+        return allPosts.data.listPosts.items as Post[];
+      } else {
+        throw new Error("Could not get posts");
+      }
+    };
+
+    fetchPostsFromApi();
+  }, []);
+
+  console.log(posts);
 
   return <Typography>Hello world</Typography>;
 }
